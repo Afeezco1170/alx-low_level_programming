daec3848279b1ae6fff33841e9da0cfa755bb850
Afeezco1170 <kazeemabdulafeez@gmail.com>
2022-10-06 12:50:55 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,13 +8,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-void *p;
-
-p = malloc(b);
-if (p == NULL)
-{
-exit(98);
-}
-
-return (p);
+	void *p;
+	p = malloc(b);
+	if (p == NULL)
+	{
+	exit(98);
+	}
+	
+	return (p);
 }
